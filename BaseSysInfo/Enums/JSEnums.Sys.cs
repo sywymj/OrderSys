7982c7a096dbb5091f7de3603b9cf308924532d7
duoduo406 <30708291+duoduo406@users.noreply.github.com>
2017-09-01 17:48:03 +0800

    V20170901

---
+++ BaseSysInfo/Enums/JSEnums.Sys.cs
@@ -0,0 +1,42 @@
+﻿using JSNet.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JSNet.BaseSys
+{
+    public enum SysCategory
+    {
+        System = 1,
+        WinForm = 2,
+        WebForm = 3,
+        WebApi = 4,
+        Mvc = 5,
+    }
+
+    public enum ResourceType
+    {
+        /// <summary>
+        /// 数据资源
+        /// </summary>
+        Data = 1,
+        /// <summary>
+        /// 菜单
+        /// </summary>
+        Menu = 2,
+        /// <summary>
+        /// 内页菜单
+        /// </summary>
+        InnerMenu = 3,
+        /// <summary>
+        /// 弹出框
+        /// </summary>
+        Popup = 4,
+        /// <summary>
+        /// 按钮
+        /// </summary>
+        Button=5,
+
+    }
+}
