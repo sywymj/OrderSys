e7f45a8d5185ba6f17efada14faeb7b5c81e72f6
duoduo406 <30708291+duoduo406@users.noreply.github.com>
2017-09-07 17:54:50 +0800

    V170907

---
+++ OrderSys.PC/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using FastJSON;
+using JSNet.BaseSys;
+using JSNet.Model;
+using JSNet.Service;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OrderSys.Admin.Controllers
+{
+    public class HomeController : AdminBaseController
+    {
+        //
+        // GET: /Home/
+        private PermissionService permissionService = new PermissionService();
+
+        public ActionResult Index()
+        {
+            return View("~/Areas/Admin/Views/Home/Index.cshtml");
+        }
+
+        public string GetLeftTree()
+        {
+            RoleEntity role = new PermissionService().GetCurrentRole();
+            DataTable dt = permissionService.GetMenu(role, "OrderSys_PC");
+            dt.Columns["Resource_ID"].ColumnName = "ID";
+            dt.Columns["Resource_ParentID"].ColumnName = "ParentID";
+            dt.Columns["Resource_FullName"].ColumnName = "Title";
+            dt.Columns["NavigateUrl"].ColumnName = "Url";
+
+            DataTable re = dt.DefaultView.ToTable(false, new string[] { "ID", "ParentID", "Title", "Url", "ImagUrl" });
+            return JSON.ToJSON(new JSResponse(re));
+        }
+
+        public string GetButton(string resourceCode)
+        {
+            RoleEntity role = new PermissionService().GetCurrentRole();
+            DataTable dt  = permissionService.GetButton(role, resourceCode);
+            return JSON.ToJSON(new JSResponse(dt));
+        }
+
+        
+    }
+}
